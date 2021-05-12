e974e510635285668412308fe3b3a6c7611115cf
MortyQ <yasenev1998@gmail.com>
2021-05-12 09:30:11 +0200

    add dropdown content

--- src/components/diseas_pages/family_training/family_training.jsx
+++ src/components/diseas_pages/family_training/family_training.jsx
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from "react";
+import {Col, Row, Container} from "react-bootstrap"
+import './family_training.css'
 
 const FamilyTraining =()=>{
 
@@ -18,8 +20,26 @@ const FamilyTraining =()=>{
 
     return(
         <div className="family_training_wrapper">
-             {exitPanel('FamilyTraining','/main','Про Нас ', '  Рекомендації з харчування')}
-                <h2>Hello</h2>
+             {exitPanel('Керівництво для сімей та професіоналів','/main','Про Нас ', '  Керівництво для сімей та професіоналів')}
+             <Container>
+                    <p className='p_respirator' >У цих двох брошурах узагальнені результати міжнародної угоди з питань лікування та організації медичної допомоги хворим м’язовою дистрофією Дюшена (МДД).</p>
+                  
+                <div className="img_respirator">
+                    <div>
+                    <p>Керівництво для сімей</p>
+                   <a target='_blank'  href="https://drive.google.com/open?id=1JI7z3lkAtWLVXN4dROYT6uOSJ4y4cmUV">
+                       <img src="http://miolife.org/wp-content/uploads/2019/06/1FamilyGuide-1-600x600.jpg" alt="alt" />
+                       </a> 
+                    </div>
+                    <div>
+                       <p>Керівництво для професіоналів</p>
+                       <a target='_blank'  href="https://drive.google.com/open?id=1RSkXv79nBfu4_2cMnp8Hs7ASYADlBr5Z">
+                    <img  src="http://miolife.org/wp-content/uploads/2019/06/2FamilyGuide-600x600.jpg" alt="alt" />
+                       </a>
+                    </div>
+                </div>
+
+</Container>
         </div>
     )
 }
--- src/components/diseas_pages/recomended_food_page/recomended_food_page.jsx
+++ src/components/diseas_pages/recomended_food_page/recomended_food_page.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import './recomended_food_page.css'
+import {Col, Row, Container} from "react-bootstrap"
 
 const RecomendedFoodPage =()=>{
 
@@ -20,7 +21,27 @@ const RecomendedFoodPage =()=>{
     return(
         <div className="recomended_food_wrapper">
              {exitPanel('Рекомендації з харчування','/main','Про Нас ', '  Рекомендації з харчування')}
-                <h2>Hello</h2>
+             <Container>
+                 <h4>Важливим аспектом при м’язової дистрофії Дюшена – є дотримання правильної дієти.</h4>
+                    <p>На сьогоднішній день, золотим стандартом медикаментозної терапії є застосування 
+                    глюкокортикостероїдів. Деякі батьки намагаються також доповнити раціон різними харчовими добавками (БАД).</p>
+                  
+                <div className="img_recomended_food">
+                    <div>
+                    <p>У цій презентації Ви зможете ознайомитись з основними принципами дієти на ранній стадії захворювання, з різними харчовими добавками і їх клінічною ефективністю.</p>
+                   <a target='_blank'  href="https://drive.google.com/open?id=1lfOjuTnsqY2F29E9b1M4d-CNHStN7itQ">
+                       <img src="http://miolife.org/wp-content/uploads/2019/06/rpp1-1-600x600.png" alt="alt" />
+                       </a> 
+                    </div>
+                    <div>
+                       <p>Вміст кальцію в продуктах харчування</p>
+                       <a target='_blank'  href="https://drive.google.com/open?id=1EAYuXaegjGj8V2m8FGRTTt4SwOefqW00">
+                    <img  src="http://miolife.org/wp-content/uploads/2019/06/rpp2-1-600x600.png" alt="alt" />
+                       </a>
+                    </div>
+                </div>
+
+</Container>
         </div>
     )
 }
